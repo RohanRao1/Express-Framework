661ae7d880a03507658edece6f123b52d2b13ddb
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-08-25 00:49:12 +0530

    routers n filters

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 
+const adminRoute = require('./routes/admin')
+const shopRoute = require('./routes/shop')
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : true}))
 
-app.use("/add-product", (req, res, next) => {
-//   console.log("in the product");
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name ='title'><input type='number' name ='size'><button type='submit'>Add Product</button> </form>"
-  );
-});
+app.use('/admin', adminRoute)
+app.use(shopRoute)
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
+app.use((req,res,next) => {
+  res.status(404).send('<h1>Page not found</h1>')
 })
-
-app.use("/", (req, res, next) => {
-//   console.log("in the middleware");
-  res.send("<h1>Hello Express world </h1>");
-});
 
 app.listen(4000);
