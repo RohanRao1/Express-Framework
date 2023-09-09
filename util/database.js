18213de11861d743790093fee374226b1c75aedd
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-09-09 13:39:16 +0530

    using sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,18 @@
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
 
-const pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    database : 'node_complete',
-    password : '123456'
-})
+// const pool = mysql.createPool({
+//     host : 'localhost',
+//     user : 'root',
+//     database : 'node_complete',
+//     password : '123456'
+// })
 
+// module.exports = pool.promise() // before using sequelize
 
-module.exports = pool.promise() 
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node_complete", "root", "123456", {
+  dialect: "mysql", host : 'localhost'
+});
+
+module.exports = sequelize
